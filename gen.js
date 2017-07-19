c098441cd914ad564cf66887547d45bf993fce35
colder <colder@vitarn.com>
2017-07-19 20:36:50 +0800

    gen es

--- gen.js
+++ gen.js
@@ -7,6 +7,9 @@ const resolve = (...p) => path.resolve(__dirname, ...p)
 const mkdir = mkdirSync
 const read = p => readFileSync(p, 'utf8')
 const write = (p, c) => console.log('WRITE', p) || writeFileSync(p, c)
+const regexp = /^export { default as (\w+) } from '\.\/([\w-]+)\/index(\.web)?';$/
+
+// lib
 
 write(
     resolve('lib', 'index.js'),
@@ -23,13 +26,66 @@ write(
 read(resolve('node_modules', 'antd-mobile', 'lib', 'index.d.ts'))
     .split(/\n/)
     .forEach(line => {
-        const match = line.match(/^export { default as \w+ } from '\.\/([\w-]+)\/index(\.web)?';$/)
+        const match = line.match(regexp)
         if (!match) return
-        const filename = match[1]
-        const websuffix = match[2]
+        const filename = match[2]
+        const websuffix = match[3]
 
         write(
             resolve('lib', `${filename}.js`),
             `module.exports = require('antd-mobile/lib/${filename}/index${websuffix || ''}')`
+        )
+    })
+
+// es
+
+write(
+    resolve('es', 'index.js'),
+    read(resolve('node_modules', 'antd-mobile', 'es', 'index.js'))
+        .replace(/'\./g, '\'antd-mobile/es')
+)
+
+// write(
+//     resolve('es', 'index.js'),
+//     read(resolve('node_modules', 'antd-mobile', 'es', 'index.js'))
+//         .split(/\n/)
+//         .map(line => line.match(regexp))
+//         .filter(match => !!match)
+//         .map(match => {
+//             const compname = match[1]
+//             const filename = match[2]
+//             const websuffix = match[3]
+//             const imp = `import ${compname} from 'antd-mobile/es/${filename}/index${websuffix || ''}';`
+//             const exp = `export { ${compname} };`
+
+//             return { imp, exp }
+//         })
+//         .reduce((acc, { imp, exp }) => {
+//             acc.push(imp)
+//             acc.push(exp)
+
+//             return acc
+//         }, [])
+//         .join('\r\n')
+// )
+
+write(
+    resolve('es', 'index.d.ts'),
+    read(resolve('node_modules', 'antd-mobile', 'es', 'index.d.ts'))
+        .replace(/'\./g, '\'antd-mobile/es')
+)
+
+read(resolve('node_modules', 'antd-mobile', 'es', 'index.js'))
+    .split(/\n/)
+    .forEach(line => {
+        const match = line.match(regexp)
+        if (!match) return
+        const compname = match[1]
+        const filename = match[2]
+        const websuffix = match[3]
+
+        write(
+            resolve('es', `${filename}.js`),
+            `import ${compname} from 'antd-mobile/es/${filename}/index${websuffix || ''}';\r\nexport default ${compname};`
         )
     })
